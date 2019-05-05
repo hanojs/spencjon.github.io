4ecafe5eaf2225555402c35541a4a45ad078e28d
Jonah <jonahlafayette@gmail.com>
2019-05-04 22:50:11 -0700

    Changes theme and general UI improvements

--- js/scripts.js
+++ js/scripts.js
@@ -16,7 +16,7 @@
     $('html').removeClass('no-js');
 
     // Animate to section when nav is clicked
-    $('header a').click(function(e) {
+    $('nav a').click(function(e) {
 
         // Treat as normal link if no-scroll class
         if ($(this).hasClass('no-scroll')) return;
@@ -30,8 +30,8 @@
         }, Math.abs(window.pageYOffset - $(heading).offset().top) / 1.5);
 
         // Hide the menu once clicked if mobile
-        if ($('header').hasClass('active')) {
-            $('header, body').removeClass('active');
+        if ($('nav').hasClass('active')) {
+            $('nav, body').removeClass('active');
         }
     });
 
@@ -78,12 +78,12 @@
 
     // Open mobile menu
     $('#mobile-menu-open').click(function() {
-        $('header, body').addClass('active');
+        $('nav, body').addClass('active');
     });
 
     // Close mobile menu
     $('#mobile-menu-close').click(function() {
-        $('header, body').removeClass('active');
+        $('nav, body').removeClass('active');
     });
 
     // Load additional projects
